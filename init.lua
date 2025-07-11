b849cddfbdfac03f6fe1c732b9ccaa4c6a4ddd23
gabriellecronofy <gabrielle@cronofy.com>
2025-07-11 11:23:47 +0100

    Remove typo

--- init.lua
+++ init.lua
@@ -583,7 +583,7 @@ require('lazy').setup({
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')init.
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
